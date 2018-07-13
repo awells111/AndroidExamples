c6443c75f4f72854841aa3dc4b606cba3477ad1e
awells111 <awells111@gmail.com>
2018-07-12 20:45:22 -0400

    Added JosephusSurvivor

---
+++ codewars/src/test/java/com/android/awells/kyu5/JosephusSurvivor/JosephusSurvivorTest.java
@@ -0,0 +1,41 @@
+package com.android.awells.kyu5.JosephusSurvivor;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*Tests written by codewars. These will only be run AFTER I have solved the problem and after I have
+created my own tests.*/
+
+public class JosephusSurvivorTest {
+
+  @Test
+  public void test1() {
+    josephusTest(7, 3, 4);
+  }
+
+  @Test
+  public void test2() {
+    josephusTest(11, 19, 10);
+  }
+
+  @Test
+  public void test3() {
+    josephusTest(40, 3, 28);
+  }
+
+  @Test
+  public void test4() {
+    josephusTest(14, 2, 13);
+  }
+
+  @Test
+  public void test5() {
+    josephusTest(100, 1, 100);
+  }
+
+  private void josephusTest(final int n, final int k, final int result) {
+    String testDescription = String.format("Testing where n = %d and k = %d", n, k);
+    assertEquals(testDescription, result, Solution.josephusSurvivor(n, k));
+  }
+}
---
+++ codewars/src/test/java/com/android/awells/kyu5/JosephusSurvivor/SolutionTest.java
@@ -0,0 +1,35 @@
+package com.android.awells.kyu5.JosephusSurvivor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import org.junit.Test;
+
+public class SolutionTest {
+
+  private static final int TEST_n = 7;
+  private static final int TEST_k = 3;
+  private static final int EXPECTED_SURVIVOR = 4;
+  private List<Integer> list = Solution.buildList(TEST_n);
+
+  @Test
+  public void buildListSize() {
+    assertEquals(TEST_n, list.size());
+  }
+
+  @Test
+  public void buildListFirst() {
+    assertTrue(list.get(0).equals(1));
+  }
+
+  @Test
+  public void buildListLast() {
+    assertTrue(list.get(TEST_n - 1).equals(TEST_n));
+  }
+
+  @Test
+  public void josephusSurvivor() {
+    assertTrue(Solution.josephusSurvivor(TEST_n, TEST_k) == EXPECTED_SURVIVOR);
+  }
+}
